1d49108f6cdd0615b88b78c5111e3e2d01aed876
Luc Perkins <lucperkins@gmail.com>
2023-04-27 13:56:57 +0200

    Inline notes in eval

--- eval/src/main.rs
+++ eval/src/main.rs
@@ -6,6 +6,50 @@ use std::io::Write;
 use std::path::PathBuf;
 use wasmtime::{Config, Engine, Module, Store};
 
+#[tokio::main]
+async fn main() -> Result<(), EvalError> {
+    let args = Cli::parse();
+    args.validate()?;
+
+    // Convert CLI inputs into JSON (serde_json::Value)
+    let data = get_json(args.data, args.data_path)?; // OPA data object
+    let input = get_json(args.input, args.input_path)?; // OPA input object
+
+    // Create a Wasm engine to handle policy.wasm
+    let mut config = Config::new();
+    config.async_support(true);
+    let engine = Engine::new(&config)?;
+    let policy_wasm = tokio::fs::read("%policy%").await?; // Nix substitutes %policy% with a Nix store path to the policy file
+    let policy_module = Module::new(&engine, policy_wasm)?;
+
+    // Create a Wasmtime store
+    let mut store = Store::new(&engine, ());
+
+    // Create a Wasmtime runtime from the store and module
+    let runtime = Runtime::new(&mut store, &policy_module).await?;
+
+    // Supply the runtime with data
+    let policy = runtime.with_data(&mut store, &data).await?;
+
+    // Evaluate the policy
+    let value: serde_json::Value = policy.evaluate(&mut store, "%entrypoint%", &input).await?;
+
+    // Convert the resulting JSON into a nice indented string
+    let output_json = serde_json::to_string_pretty(&value)?;
+
+    // Write the result either to a file or to stdout
+    if let Some(output) = args.output {
+        let path = output.to_str().unwrap();
+        let mut file = File::create(path)?;
+        file.write_all(output_json.as_bytes())?;
+    } else {
+        std::io::stdout().write_all(output_json.as_bytes())?;
+    }
+
+    Ok(())
+}
+
+
 /// A policy evaluator wrapping the OPA policy %rego% with entrypoint %entrypoint%
 #[derive(Parser)]
 #[command(author, about, long_about = None)]
@@ -64,6 +108,8 @@ enum EvalError {
     Json(#[from] serde_json::Error),
 }
 
+// Helper function for producing JSON from either a raw string or a filepath
+// If neither is supplied, an empty JSON object is returned
 fn get_json(s: Option<String>, p: Option<PathBuf>) -> Result<Value, EvalError> {
     let js = if let Some(s) = s {
         serde_json::from_str(&s)?
@@ -74,33 +120,4 @@ fn get_json(s: Option<String>, p: Option<PathBuf>) -> Result<Value, EvalError> {
         json!({})
     };
     Ok(js)
-}
-
-#[tokio::main]
-async fn main() -> Result<(), EvalError> {
-    let args = Cli::parse();
-    args.validate()?;
-
-    let mut config = Config::new();
-    config.async_support(true);
-    let engine = Engine::new(&config)?;
-    let module = tokio::fs::read("%policy%").await?;
-    let module = Module::new(&engine, module)?;
-    let mut store = Store::new(&engine, ());
-    let data = get_json(args.data, args.data_path)?;
-    let input = get_json(args.input, args.input_path)?;
-    let runtime = Runtime::new(&mut store, &module).await?;
-    let policy = runtime.with_data(&mut store, &data).await?;
-    let value: serde_json::Value = policy.evaluate(&mut store, "%entrypoint%", &input).await?;
-    let output_json = serde_json::to_string_pretty(&value)?;
-
-    if let Some(output) = args.output {
-        let path = output.to_str().unwrap();
-        let mut file = File::create(path)?;
-        file.write_all(output_json.as_bytes())?;
-    } else {
-        std::io::stdout().write_all(output_json.as_bytes())?;
-    }
-
-    Ok(())
 }
